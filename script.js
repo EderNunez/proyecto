b4e918a3c289ce353cd6b2ebbd6600f2839951e9
EderNunez <163679489+EderNunez@users.noreply.github.com>
2025-03-02 14:41:30 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-if (rol != null) {
-  const asistenciaLink = document.getElementById("asistencia-link");
-  if (rol !== "ADMINISTRADOR") {
-    asistenciaLink.setAttribute("href", "#");
-    asistenciaLink.onclick = () => {
-      alert("No tienes permiso para acceder a esta página");
-    };
-  }
-}
